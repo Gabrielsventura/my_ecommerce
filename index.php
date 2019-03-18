5475faa61258f736068dfd09f140eeb487223aba
Gabriel Ventura <gaventura-1995@hotmail.com>
2019-03-18 17:25:39 -0300

    classe page criada

--- index.php
+++ index.php
@@ -1,19 +1,30 @@
 <?php 
 
-require_once("vendor/autoload.php"); //require autoload composer
+require_once("vendor/autoload.php"); //require autoload composer, tras as dependencias
 
-$app = new \Slim\Slim();//instancia do slim framework
+use \Slim\Slim;
+use \Principal\Page;
+
+$app = new Slim();//instancia do slim framework
 
 $app->config('debug', true); //detalha os erros que acontecem
 
+
+//a unica coisa que muda, são as configuração dentro rota
 $app->get('/', function() { //rota principal
     
-	$sql = new Principal\DB\Sql();
+    $page = new Page();//instancia da classe Page
+
+    $page->setTpl("index");//direcionando para o index
+	
+    
+
+	/*$sql = new Principal\DB\Sql();
 
 	$result = $sql->select("SELECT * FROM tb_users");
 
 	echo json_encode($result);
-
+    */
 });
 
 $app->run(); //executa tudo
---
+++ vendor/classes/php-classes/src/Page.php
@@ -0,0 +1,63 @@
+<?php 
+
+namespace Principal;
+
+use Rain\Tpl;
+
+class Page{
+
+	private $tpl;
+	private $options = [];
+	private $defaults =  ["data"=>[]];
+
+	public function __construct($opts = array()){
+
+		$this->options = array_merge($this->defaults, $opts); //para o opts sobrescrever o default
+
+		$config = array(
+					"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",//caminho do template
+					"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache",
+					"debug"         => false
+				   );
+
+	Tpl::configure( $config );
+
+	$this->tpl = new Tpl;
+
+	$this->setData($this->options["data"]);
+
+	 $this->tpl->draw("header"); //desenha o template na tela
+	
+
+	}
+
+	public function setData($data = array()){//função para nao precisar fazer o foreach nos metodos e so chamar
+
+		foreach($data as $key => $value){//precorre os dados do template
+		$this->tpl->assign($key, $value);
+
+     }
+	
+	}
+
+	public function setTpl($name, $data = array(), $returnHtml = false){
+
+		$this->setData($data);
+
+		return $this->tpl->draw($name, $returnHtml);//retorna o draw para o tpl
+     }
+
+
+	
+
+
+	public function __destruct(){
+
+         $this->tpl->draw("footer");//desneha o footer
+
+
+	}
+}
+
+
+?>
